dbc4917b8f730f3fa38e8b7bde8a3040119273ff
MPCarranza <mariapilarcarranza@gmail.com>
2024-11-07 14:22:25 -0300

    fix deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist", // Esto asegura que Vite genera los archivos optimizados en la carpeta 'dist'
-    rollupOptions: {
-      input: "/public/index.html", // Asegúrate de que Vite sepa que el archivo principal es 'index.html' en 'public'
-    },
-  },
+  base: "./", // Si usas rutas relativas, esto puede ayudar
 });
